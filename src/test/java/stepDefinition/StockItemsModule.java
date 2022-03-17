c0439d294c1bb65655c854551a29d3b02af95c87
ramprasadsuthi <59646329+ramprasadsuthi@users.noreply.github.com>
2022-03-17 12:20:54 +0530

    Update StockItemsModule.java

--- src/test/java/stepDefinition/StockItemsModule.java
+++ src/test/java/stepDefinition/StockItemsModule.java
@@ -2,4 +2,7 @@ package stepDefinition;
 
 public class StockItemsModule {
 
+       public void deleteStockItem() {
+          System.out.println("This is place holder for deleting a stock item");
+       }
 }
